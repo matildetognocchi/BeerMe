af3df56ffbe2df5b0938c7b14ab9f4dba15f643c
Matilde Tognocchi <matilde.tog@gmail.com>
2024-01-30 13:04:32 +0100

    First Commit of BeerMe UiKit version

---
+++ BeerMeUiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerMeUiKit
+//
+//  Created by Matilde Tognocchi on 29/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
