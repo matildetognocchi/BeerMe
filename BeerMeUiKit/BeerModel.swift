82edf58b1e19cdb4b48faa9305b5b7fd7626a0e4
Matilde Tognocchi <matilde.tog@gmail.com>
2024-02-21 20:06:13 +0100

    Create a Model - BeerModel

--- BeerMeUiKit/BeerModel.swift
+++ BeerMeUiKit/BeerModel.swift
@@ -1,8 +1,16 @@
 //
-//  beerModel.swift
+//  BeerModel.swift
 //  BeerMeUiKit
 //
 //  Created by Matilde Tognocchi on 21/2/24.
 //
 
 import Foundation
+
+struct BeerModel: Decodable {    
+    
+    let id: Int
+    let uid, brand, name, style: String
+    let hop, yeast, malts, ibu: String
+    let alcohol, blg: String
+}
